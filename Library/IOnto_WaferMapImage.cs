e91304e5b9d16151fda2ea6b1f7d91dc12840c13
greatit-lab <gil2zum@gmail.com>
2025-09-10 10:42:05 +0900

    Update IOnto_WaferMapImage.cs

--- Library/IOnto_WaferMapImage.cs
+++ Library/IOnto_WaferMapImage.cs
@@ -277,6 +277,7 @@ namespace Onto_WaferMapImageLib
                 using (var cmd = new NpgsqlCommand(sql, conn))
                 {
                     DateTime serv_kst = TimeSyncProvider.Instance.ToSynchronizedKst(fileDateTime);
+                    serv_kst = new DateTime(serv_kst.Year, serv_kst.Month, serv_kst.Day, serv_kst.Hour, serv_kst.Minute, serv_kst.Second);
 
                     cmd.Parameters.AddWithValue("@eqpid", eqpid);
                     cmd.Parameters.AddWithValue("@datetime", fileDateTime);
